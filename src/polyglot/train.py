19867da1380a2213ec59161110374e0f0e082e16
hipster4020 <hipster4020@gmail.com>
2022-10-24 12:02:38 +0000

    train 수정

--- src/polyglot/train.py
+++ src/polyglot/train.py
@@ -5,17 +5,17 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 def main(cfg):
     # tokenizer
     tokenizer = AutoTokenizer.from_pretrained(cfg.MODEL.model_name)
-    print(f"tokenizer : {tokenizer}")
 
     test_sentence = "안녕하세요. 테스트입니다."
-    print(f"encode : {tokenizer.encode(test_sentence)}")
-    print(f"tokenize : {tokenizer.tokenize(test_sentence)}")
+    input_ids = tokenizer(test_sentence, return_tensors="pt").input_ids
 
-    print(f"decode : {tokenizer.decode(tokenizer.encode(test_sentence))}")
-    
+
+    print(f"input_ids : {input_ids}")
+
     # model
     model = AutoModelForCausalLM.from_pretrained(cfg.MODEL.model_name)
-    print(f"model : {model}")
+    print(model(input_ids))
+
 
 
 
